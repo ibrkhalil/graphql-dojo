d79e029a0c1908937844bda8486705c3e6a1b60d
ibrkhalil <vampirekid017@gmail.com>
2024-06-02 17:24:37 +0300

    Initial setup of two schemas and solve circular dependency using JS Closures

--- schema/schema.js
+++ schema/schema.js
@@ -1,14 +1,26 @@
+const axios = require("axios");
 const graphql = require("graphql");
-const _ = require('lodash')
 
-const { GraphQLObjectType, GraphQLInt, GraphQLString } = graphql;
+const { GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLList } = graphql;
 
-const users = [{
-    id: '23', firstName: 'Bill', age: 20
-},
-{
-    id: '47', firstName: 'Samantha', age: 21
-}]
+const CompanyType = new GraphQLObjectType({
+  name: "Company",
+  // Closure that'll be only ran after the rest of the file
+  // Has done executing
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    users: {
+      type: new GraphQLList(UserType),
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/companies/${parentValue.id}/users`)
+          .then((response) => response.data);
+      }
+    }
+  })
+});
 
 const UserType = new GraphQLObjectType({
   name: "User",
@@ -16,6 +28,14 @@ const UserType = new GraphQLObjectType({
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
+    company: {
+      type: CompanyType,
+      resolve(parentValue, _args) {
+        return axios
+          .get(`http://localhost:3000/companies/${parentValue.companyId}`)
+          .then((response) => response.data);
+      },
+    },
   },
 });
 
@@ -30,13 +50,27 @@ const RootQuery = new GraphQLObjectType({
         },
       },
       resolve(_parentValue, args) {
-            return _.find(users, {id: args.id});
+        return axios
+          .get(`http://localhost:3000/users/${args.id}`)
+          .then((response) => response.data);
+      },
+    },
+    company: {
+      type: CompanyType,
+      args: {
+        id: {
+          type: GraphQLString,
+        },
+      },
+      resolve(_parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/companies/${args.id}`)
+          .then((response) => response.data);
       },
     },
   },
 });
 
-
 module.exports = new graphql.GraphQLSchema({
-    query: RootQuery
-})
+  query: RootQuery,
+});
